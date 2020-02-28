f5cff443d00fceaf581b57ea5f025f9e7eeb975d
Laura Kroon-Verschuren <42880168+LauraKroonVerschuren@users.noreply.github.com>
2020-02-28 11:19:19 +0100

    Show foto v0.2.a

--- js/main.js
+++ js/main.js
@@ -1,31 +1,82 @@
+// Foto zoeken --------------------------------
+
 $(document).ready(function() {
-    let fotolijst = [];
+    let version = '0.2.a',
+    fotolijst = [];
     var menuItem = $('#menu ul li');
 
     $('#gallery').show();
+    $('#footer').html('Laura Kroon-Verschuren - versie' + ' ' + version);
 
-// get JSon data met jquery GET
-        $.get("data/fotos.json", function(data, status){
-            let aantalFotos = data.fotos.length;
-            let htmlStr = '';
-// looop door alle foto's heen
-            for (let i = 0; i < aantalFotos; i++) {
-                fotolijst[i] = data.fotos[i];                           // copy foto in array
 
-                // maak HTML string
-                htmlStr += '<div class="foto-box">';
-                htmlStr += '<img src="images/small_' + fotolijst[i].url + '" width="" height="" alt="" class="thumb" />';
-                htmlStr += '</div>';
-            }
+    $.get("data/fotos.json", function(data, status){
+        let aantalFotos = data.fotos.length;
+        let htmlStr = '';
 
-            $('#gallery .foto-row').html(htmlStr); // set htmlStr in site
-            
-            console.log('fotos', data, status, data.fotos[1].url, aantalFotos);
-       
-        });
+        for (let i = 0; i < aantalFotos; i++) {
+            fotolijst[i] = data.fotos[i];                       
+
+
+            htmlStr += '<div class="foto-box">';
+            htmlStr += '<img src="images/small_' + fotolijst[i].url + '" width="" height="" alt="" class="thumb" />';
+            htmlStr += '</div>';
+        }
+
+        $('#gallery .foto-row').html(htmlStr); 
+        
+        console.log('fotos', data, status, data.fotos[1].url, aantalFotos);
+    
+    });
     
 
-    // console.log('READY', menuItem, fotolijst)
+// Foto selecteren --------------------------------
+        
+    $('#gallery').on('click', function(evt) {
+
+        let fotoSelect = evt.target,
+            fotoSrc = $(fotoSelect).attr('src');
+
+        if (fotoSrc != undefined) {
+            let lastSlash = fotoSrc.lastIndexOf('/');
+                fotoSrc = fotoSrc.substr(lastSlash + 7); // 7 = 1 plus lengte van small_
+            showFoto(fotoSrc);
+
+        }
+
+        // console.log('foto select', fotoSelect, fotoSrc, lastSlash);
+    })   
+
+
+
+    $('#fotoblok').on('click', function(){
+            $('#fotoblok').slideUp(500);
+
+            console.log('sluit');
+    });
+
+
+    function showFoto(fotoSrc) {
+            let fotoUrl = 'images/' + fotoSrc;
+
+            $('#fotoblok').slideDown(1000);
+
+            $('#fotoblok img').attr('src', fotoUrl);
+
+      //  console.log('showfoto', fotoSrc, fotoUrl);
+    }
+
+
+
+
+
+
+
+
+
+
+
+// Menu--------------------------------
+
 
     $('#menu ul li').on('click', function(evt) {
         // haal de class er uit met jQuery met een $(this).attr('NAAM VAN CLASSSSSSSSSSSSSS')
